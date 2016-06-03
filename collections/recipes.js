bf663104cb380007b792149ea8783ee876753ff9
Matt Bennett <matt.bennett4478@gmail.com>
2016-06-03 11:19:24 -0500

    Start

---
+++ collections/recipes.js
@@ -0,0 +1,52 @@
+Recipes = new Mongo.Collection('recipes');
+
+Recipes.allow({
+	insert: function(userId, doc) {
+		return !!userId;
+	}
+});
+
+Ingredient = new SimpleSchema({
+	name: {
+		type: String
+	},
+	amount: {
+		type: String
+	}
+});
+
+RecipeSchema = new SimpleSchema({
+	name: {
+		type: String,
+		label: "Name"
+	},
+	description: {
+		type: String,
+		label: "Description"
+	},
+	ingredients: {
+		type: [Ingredient]
+	},
+	author: {
+		type: String,
+		label: "Author",
+		autoValue: function() {
+			return this.userId
+		},
+		autoform: {
+			type: "hidden"
+		}
+	},
+	created_at: {
+		type: Date,
+		label: "Created At",
+		autoValue: function() {
+			return new Date()
+		},
+		autoform: {
+			type: "hidden"
+		}
+	}
+});
+
+Recipes.attachSchema(RecipeSchema);
