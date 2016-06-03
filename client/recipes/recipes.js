d5ec1e059f1f5b3af77ae2c07993a6e7286247c1
Matt Bennett <matt.bennett4478@gmail.com>
2016-06-03 14:26:12 -0500

    added a view details

--- client/recipes/recipes.js
+++ client/recipes/recipes.js
@@ -1,1 +1,12 @@
-Meteor.subscribe('recipes');
+Template.recipes.onCreated(function(){
+	var self = this;
+	self.autorun(function() {
+		self.subscribe('recipes');
+	});
+});
+
+Template.recipes.helpers({
+	recipes: ()=> {
+		return Recipes.find({});
+	}
+})
